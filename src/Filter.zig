d49e7aab6559e7b89c1e84b4c97ab972611c61d3
Elyas Bianchi <elyasb99@gmail.com>
2026-01-17 10:22:48 -0800

    work

--- src/Filter.zig
+++ src/Filter.zig
@@ -5,7 +5,6 @@ const Filter = @This();
 contents: []u8,
 
 pub fn init(filter_path: []const u8) !?Filter {
-    var buf: [1024]u8 = undefined;
     const file = std.fs.cwd().openFile(filter_path, .{}) catch |err| switch (err) {
         error.FileNotFound => {
             std.log.err("Can't find provided filter config file: {s}", .{filter_path});
@@ -14,6 +13,7 @@ pub fn init(filter_path: []const u8) !?Filter {
         else => return err,
     };
 
+    var buf: [1024]u8 = undefined;
     const n = try file.read(&buf);
 
     return .{ .contents = buf[0..n] };
